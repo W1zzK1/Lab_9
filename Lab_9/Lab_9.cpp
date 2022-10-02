7d3277f0e59562f1bd98292feabd281d6b593036
W1zzA <W1zzA@W1ZZK1>
2022-10-02 22:31:18 +0300

    Добавьте файлы проекта.

---
+++ Lab_9/Lab_9.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    const int lim = 5;
+
+    static int a[lim][lim];
+
+    for (int i = 0; i < lim; ++i)
+        for (int j = 0; j < lim; ++j)
+        {
+            cout << "A[" << i << "][" << j << "]:";
+            cin >> a[i][j];
+        }
+    int result = 0;
+    for (int j = 0; j < lim; ++j)
+    {
+        int i;
+        for (i = 0; i < lim; ++i)
+            if (a[i][j] % 2 == 0) break;
+        if (i == lim)
+        {
+            result = j;
+            break;
+        }
+    }
+    cout << "Row: " << result << endl;
+    return 0;
+}
